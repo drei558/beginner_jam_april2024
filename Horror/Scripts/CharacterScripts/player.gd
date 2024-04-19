0c7e5af2ae847511f2244263a037f4d288419c92
Foppyx <166410160+Foppyx@users.noreply.github.com>
2024-04-19 20:13:09 +0500

    Le horror

---
+++ Horror/Scripts/CharacterScripts/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var speed = 300
+
+var lastAnimDir: String = "Down"
+
+func get_input():
+	look_at(get_global_mouse_position())
+	var input_direction = Input.get_vector("Walk_Left", "Walk_Right", "Walk_Up", "Walk_Down")
+	velocity = input_direction * speed
+
+func _physics_process(_delta):
+	get_input()
+	move_and_slide()
+	if velocity.length() != 0:
+		$AnimationPlayer.play("Walking")
+	else:
+		$AnimationPlayer.stop()
